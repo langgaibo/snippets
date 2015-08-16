d636c5b1a27309f6f2977d7eabe3836ee5851fbc
langgaibo <gabe.langton@gmail.com>
2015-08-16 13:39:45 -0700

    no need to hash login

--- pw_check.py
+++ pw_check.py
@@ -3,32 +3,33 @@
 class Validate_Creds(object):
     def __init__(self, login, pw):
         import hashlib
-        self.login = hashlib.sha256(login).hexdigest()
+        self.login = login
         self.pw = hashlib.sha256(pw).hexdigest()
 
     def retrieve_actual(self, source):
-        # from elsewhere, import creds
-        self.t_login = source.username
-        self.t_pw = source.password
+        # from elsewhere, import creds.
+        # expects sha256 hashed values!
+        self.true_login = source.username
+        self.true_pw = source.password
 
     def compare_creds(self):
-        if self.login == self.t_login:
-            if self.pw == self.t_pw:
+        if self.login == self.true_login:
+            if self.pw == self.true_pw:
                 return True
             else:
-                self.t_login = None
-                self.t_pw = None
+                self.true_login = None
+                self.true_pw = None
                 return False
         else:
-            self.t_login = None
-            self.t_pw = None
+            self.true_login = None
+            self.true_pw = None
             return False
 
 # A dummy for our testing purposes
 class Actual_Creds(object):
     def __init__(self):
         import hashlib
-        self.username = hashlib.sha256('Darth').hexdigest()
+        self.username = 'Darth'
         self.password = hashlib.sha256('Vader').hexdigest()
 
 # make it work
